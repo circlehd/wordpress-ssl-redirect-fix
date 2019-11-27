a038e026ad82f25a1be73d2526e4c1e29d008445
CircleHD Dev <dev@circlehd.com>
2019-11-27 03:45:19 -0800

    Initial release

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name: Wordpress SSL redirect fix
+ * Plugin URI: https://github.com/circlehd/wordpress-ssl-redirect-fix
+ * Description: Fix for wordpress redirect loop under reverse proxies
+ * Version: 1.0.0
+ * Text Domain: ssl-redirect-fix
+ * Author: circlehd
+ * Author URI: https://www.circlehd.com
+ */
+
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $_SERVER['HTTPS'] = 'on';
+}
+?>
---
+++ readme.md
@@ -0,0 +1,15 @@
+# wordpress-ssl-redirect-fix
+Fix for wordpress redirect loop under reverse proxies
+
+License: MIT
+License URI: https://github.com/circlehd/wordpress-ssl-redirect-fix/blob/master/LICENSE
+Tags: AWS Cloudfront, CloudFlare, SSL, redirect loops
+Requires at least: 5.0
+Requires PHP: 5.2
+Recommended PHP: 7.0
+Tested up to: 5.3
+Stable tag: 1.0.0
+
+## release notes
+### v1.0
+First Release
